088f252a7d544d966c67a5f368beeed1fa2b912f
Paulo Beckhauser <pauloricardobeckhauser@gmail.com>
2025-10-16 22:46:15 +0200

    First TinyML model

---
+++ tinyLllama.py
@@ -0,0 +1,9 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, 
+    torch_dtype=torch.float16, 
+    device_map="auto"
+)
